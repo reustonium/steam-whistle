eca8a6e7ca1dd2d5204f2a8fbe7a6145c37fca00
reustonium <andrew.ruestow@gmail.com>
2015-01-12 14:23:22 -0500

    basic app, testing heroku since foreman is broken

---
+++ app.js
@@ -0,0 +1,57 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express');
+var compress = require('compression');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var errorHandler = require('errorhandler');
+var path = require('path');
+var connectAssets = require('connect-assets');
+
+/**
+ * Controllers (route handlers).
+ */
+var homeController = require('./controllers/home');
+
+/**
+ * API keys and Passport configuration.
+ */
+var secrets = require('./config/secrets');
+
+/**
+ * Create Express server.
+ */
+var app = express();
+
+/**
+ * Express configuration.
+ */
+app.set('port', process.env.PORT);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(compress());
+app.use(connectAssets({
+  paths: [path.join(__dirname, 'public/css'), path.join(__dirname, 'public/js')]
+}));
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+
+/**
+ * Primary app routes.
+ */
+app.get('/', homeController.index);
+
+/**
+ * Error Handler.
+ */
+app.use(errorHandler());
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port %d', app.get('port'));
+});
+
+module.exports = app;
